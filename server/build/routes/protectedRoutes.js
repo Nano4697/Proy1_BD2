6d35f825dce2cb8a31a15657d58e01bf149af807
DavidMR13840 <david.monge@live.com>
2020-06-21 15:43:21 -0600

    cambio de nombre de rutas protegidas

--- server/build/routes/protectedRoutes.js
+++ server/build/routes/protectedRoutes.js
@@ -6,7 +6,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = require("express");
 const router = express_1.Router();
 const passport_1 = __importDefault(require("passport"));
-router.get('/special', passport_1.default.authenticate('jwt', { session: false }), (req, res) => {
+router.get('/protected', passport_1.default.authenticate('jwt', { session: false }), (req, res) => {
     res.send('success');
 });
 exports.default = router;
--- server/src/routes/protectedRoutes.ts
+++ server/src/routes/protectedRoutes.ts
@@ -3,7 +3,7 @@ const router = Router();
 
 import passport from 'passport'
 
-router.get('/special',passport.authenticate('jwt', {session: false}),(req,res) => {
+router.get('/protected',passport.authenticate('jwt', {session: false}),(req,res) => {
     res.send('success');
 })
 
